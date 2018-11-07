bcc495ee628e5c034c5f795dd3db4f4022df507b
Rodrigo Luis <roidersp@gmail.com>
2018-11-07 13:40:16 -0600

    generando componentes de tabs

---
+++ src/components/tabs/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import TabsItem from './tabItem'
+
+class Tabs extends Component {
+  render () {
+    return (
+      <div className='main' >
+        <header>
+          <h1>Cypto currency</h1>
+        </header>
+        <main />
+      </div>
+    )
+  }
+}
+
+export default Tabs
---
+++ src/components/tabs/tabItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class TabsItem extends Component {
+  render () {
+    return (
+      <div className='tabsContainer' />
+    )
+  }
+}
+
+export default TabsItem
